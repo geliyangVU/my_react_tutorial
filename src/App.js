207773897221524ec0aa09f828e88d26db540f8f
Geli Yang <gyang5@villanova.edu>
2022-04-20 20:17:36 -0400

    react tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      count : 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+
+  handleClick(){
+    this.setState(prevState => {
+      return  {
+        count: prevState+1
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <p>THis is the start of app</p>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick()}>change!</button>
+      </div>
+    )
+  }
 }
 
 export default App;
